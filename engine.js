e7fb396dbbdc4b27772cd2593e0ae4cf6eecc31a
Jose Carlos Recuero <jose.recuero@gmail.com>
2015-11-13 12:14:14 -0800

    Engine new and add methods
    Create newElement and addElement in framework in order to create or add
    elements to different subjects.
    
    Objects have their own id and engine has a different id.

--- engine.js
+++ engine.js
@@ -1,124 +1,106 @@
 function Engine() {
-    this.ID = 0;
-    this.getNextId = function() {
-        var newId = this.ID;
-        this.ID++;
-        return newId;
+    this.getNextEngId = function() {
+        Engine.ID++;
+        return Engine.ID;
     };
 
-    var lookForIn = function(name, table) {
-        if (name in table) {
-            var index = table.indexOf(name);
-            return table[index];
+    var lookForIn = function(id, table) {
+        if (id && (id !== 0)) {
+            for (var i = 0; i < table.length; i++) {
+                if (table[i].engId == id) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            return true;
         }
-        return null;
     };
 
     this.actors = [];
-    this.lookForActor = function(name) {
-        return lookForIn(name, this.actors);
-    };
-    this.newActor = function(name) {
-        if (this.lookForActor(name)) {
-            return null;
-        } else {
-            var anActor = new Actor(name);
-            this.actors.push(anActor);
-            return anActor;
-        }
-    };
-
     this.scenes = [];
-    this.lookForScene = function(name) {
-        return lookForIn(name, this.scenes);
-    };
-    this.newScene = function(name) {
-        if (this.lookForScene(name)) {
-            return null;
-        } else {
-            var aScene = new Scene(name);
-            this.scenes.push(aScene);
-            return aScene;
-        }
-    };
-
     this.objetos = [];
-    this.lookForObjeto = function(name) {
-        return lookForIn(name, this.objetos);
-    };
-    this.newObjeto = function(name) {
-        if (this.lookForObjeto(name)) {
-            return null;
-        } else {
-            var anObjeto = new Objeto(name);
-            this.objetos.push(anObjeto);
-            return newObjeto;
-        }
-    };
-
     this.eventos = [];
-    this.lookForEvento = function(name) {
-        return lookForIn(name, this.eventos);
+    this.actions = [];
+
+    this.elementTable = {
+        actor: {table: this.actors, klass: Actor},
+        scene: {table: this.scenes, klass: Scene},
+        objeto: {table: this.objetos, klass: Objeto},
+        evento: {table: this.eventos, klass: Evento},
+        action: {table: this.actions, klass: Action}
     };
-    this.newEvento = function(name) {
-        if (this.lookForEvento(name)) {
-            return null;
+
+    this.isElement = function (subject, element) {
+        if (subject in this.elementTable) {
+            elemTable = this.elementTable[subject];
+            return lookForIn(element.engId, elemTable.table);
         } else {
-            var anEvento = new Evento(name);
-            this.eventos.push(anEvento);
-            return anEvento;
+            return null;
         }
     };
 
-    this.actions = [];
-    this.lookForAction = function(name) {
-        return lookForIn(name, this.actions);
+    this.newElement = function (subject, args) {
+        elemTable = this.elementTable[subject];
+        var newElem = new elemTable.klass(args);
+        newElem.engId = this.getNextEngId();
+        elemTable.table.push(newElem);
+        return newElem;
     };
-    this.newAction = function(name) {
-        if (this.lookForAction(name)) {
+
+    this.addElement = function(subject, element) {
+        elemTable = this.elementTable[subject];
+        if (this.isElement(element.engId, elemTable.table)) {
             return null;
         } else {
-            var anAction = new Action(name);
-            this.actions.push(anAction);
-            return anAction;
+            if (element.engId === undefined) {
+                element.engId = this.getNextEngId();
+            }
+            elemTable.table.push(element);
+            return element;
         }
     };
 }
-
+Engine.ID = 0;
 var geng = new Engine();
 
 function GObject() {
     this.name = arguments[0] ? arguments[0] : 'object';
-    this.ID = geng.getNextId();
+    this.getNextObjId = function() {
+        GObject.ID++;
+        return GObject.ID;
+    };
+    this.objId = this.getNextObjId();
 }
+GObject.ID = 0;
 
 function inheritKlass(parent, child) {
     child.prototype = new parent();
     child.prototype.constructor = child;
 }
 
-function Actor() {
-    GObject.apply(this, arguments);
+function Actor(args) {
+    GObject.apply(this, args);
 }
 inheritKlass(GObject, Actor);
 
-function Scene() {
-    GObject.apply(this, arguments);
+function Scene(args) {
+    GObject.apply(this, args);
 }
 inheritKlass(GObject, Scene);
 
-function Objeto() {
-    GObject.apply(this, arguments);
+function Objeto(args) {
+    GObject.apply(this, args);
 }
 inheritKlass(GObject, Objeto);
 
-function Evento() {
-    GObject.apply(this, arguments);
+function Evento(args) {
+    GObject.apply(this, args);
 }
 inheritKlass(GObject, Evento);
 
-function Action() {
-    GObject.apply(this, arguments);
+function Action(args) {
+    GObject.apply(this, args);
 }
 inheritKlass(GObject, Action);
 
