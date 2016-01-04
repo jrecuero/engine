4378e39a19e58931288363f9428c63fe4b837db0
Jose Carlos Recuero <jose.recuero@gmail.com>
2016-01-04 13:22:03 -0800

    Optimize canvas and math

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -1,150 +1,140 @@
-function getPositionInPath(path, segment, t) {
-    var f = path[segment].f;
-    var args = path[segment].args.slice();
-    args.unshift(t);
-    var result = f.apply(null, args);
-    return result;
-}
-
 var nbrCars = 4;
 var paths = [];
 var steps = [];
-var position = [];
-var colors = ["red", "blue", "green", "cyan", "yellow"];
+var cars = [];
+var colors = [ "red", "blue", "green", "cyan", "yellow" ];
+var specs = [ { speed: 6 }, { speed: 5 }, { speed: 6 }, { speed: 4 } ];
+
+function getPositionInPath( path, segment, t ) {
+    var f = path[ segment ].f;
+    var args = path[ segment ].args.slice();
+    args.unshift( t );
+    var result = f.apply( null, args );
+    return result;
+}
 
 function createVars() {
-    for (var i = 0; i < nbrCars; i++) {
-        paths.push([]);
-        position.push({segment: 0, t: 0.0, color: colors[i], lap: 0});
+    for ( var i = 0; i < nbrCars; i++ ) {
+        paths.push( [] );
+        cars.push( { segment: 0,
+                     t: 0.0,
+                     color: colors[ i ],
+                     lap: 0,
+                     spec: specs[ i ] } );
     }
 }
 
-function createPath(p) {
+function createPath( p ) {
     var origin, next, x;
     var C1, C2, C3, C4;
     var diff = 10 * p;
     steps = [];
 
-    C1 = Point(100, 10 + diff);
-    C2 = Point(200, 10 + diff);
-    paths[p].push({f: getLineBezier, args: [C1, C2]});
-    steps.push(0.02);
-    // steps.push(0.01);
+    C1 = Point( 100, 10 + diff );
+    C2 = Point( 200, 10 + diff );
+    paths[ p ].push( { f: getLineBezier, args: [ C1, C2 ] } );
+    steps.push( 0.02 );
 
-    C1 = Point(200, 10 + diff);
-    C2 = Point(350 - diff, 0 + diff);
-    C3 = Point(450 - diff, 200 - diff);
-    C4 = Point(300, 200 - diff);
-    paths[p].push({f: getCubicBezier, args: [C1, C2, C3, C4]});
-    steps.push(0.005);
-    // steps.push(0.01);
+    // Steps.push(0.01);
 
-    C1 = Point(300, 200 - diff);
-    C2 = Point(100, 200 - diff);
-    C3 = Point(100, 400 - diff);
-    C4 = Point(50 + diff, 100);
-    paths[p].push({f: getCubicBezier, args: [C1, C2, C3, C4]});
-    steps.push(0.005);
-    // steps.push(0.01);
+    C1 = Point( 200, 10 + diff );
+    C2 = Point( 350 - diff, 0 + diff );
+    C3 = Point( 450 - diff, 200 - diff );
+    C4 = Point( 300, 200 - diff );
+    paths[ p ].push( { f: getCubicBezier, args: [ C1, C2, C3, C4 ] } );
+    steps.push( 0.005 );
 
-    C1 = Point(50 + diff, 100);
-    C2 = Point(50 + diff, 100);
-    C3 = Point(30 + diff , 10 + diff);
-    C4 = Point(102, 10 + diff);
-    paths[p].push({f: getCubicBezier, args: [C1, C2, C3, C4]});
-    steps.push(0.02);
-    // steps.push(0.01);
+    // Steps.push(0.01);
+
+    C1 = Point( 300, 200 - diff );
+    C2 = Point( 100, 200 - diff );
+    C3 = Point( 100, 400 - diff );
+    C4 = Point( 50 + diff, 100 );
+    paths[ p ].push( { f: getCubicBezier, args: [ C1, C2, C3, C4 ] } );
+    steps.push( 0.005 );
+
+    // Steps.push(0.01);
+
+    C1 = Point( 50 + diff, 100 );
+    C2 = Point( 50 + diff, 100 );
+    C3 = Point( 30 + diff, 10 + diff );
+    C4 = Point( 102, 10 + diff );
+    paths[ p ].push( { f: getCubicBezier, args: [ C1, C2, C3, C4 ] } );
+    steps.push( 0.02 );
+
+    // Steps.push(0.01);
 }
 
-function drawPath(ctx, path) {
-    var origin, next, x;
-    var C1, C2, C3, C4;
-
+function drawPath( ctx, path ) {
     ctx.beginPath();
     ctx.strokeStyle = "black";
-    C1 = path[0].args[0];
-    C2 = path[0].args[1];
-    ctx.moveTo(C1.x, C1.y);
-    drawLineBezier(ctx, C1, C2);
-
-    C1 = path[1].args[0];
-    C2 = path[1].args[1];
-    C3 = path[1].args[2];
-    C4 = path[1].args[3];
-    drawCubicBezier(ctx, C1, C2, C3, C4);
-
-    C1 = path[2].args[0];
-    C2 = path[2].args[1];
-    C3 = path[2].args[2];
-    C4 = path[2].args[3];
-    drawCubicBezier(ctx, C1, C2, C3, C4);
-
-    C1 = path[3].args[0];
-    C2 = path[3].args[1];
-    C3 = path[3].args[2];
-    C4 = path[3].args[3];
-    drawCubicBezier(ctx, C1, C2, C3, C4);
-
+    ctx.moveTo( path[ 0 ].args[ 0 ].x, path[ 0 ].args[ 1 ].y );
+    drawBezier( ctx, getLineBezier, path[ 0 ].args );
+    drawBezier( ctx, getCubicBezier, path[ 1 ].args );
+    drawBezier( ctx, getCubicBezier, path[ 2 ].args );
+    drawBezier( ctx, getCubicBezier, path[ 3 ].args );
     ctx.stroke();
 }
 
-function setRaceInfo(msg) {
-    var info = document.getElementById("info");
+function setRaceInfo( msg ) {
+    var info = document.getElementById( "info" );
     info.innerHTML = msg;
 }
 
-function drawPaths(ctx) {
-    for (var i = 0; i < nbrCars; i++) {
-        drawPath(ctx, paths[i]);
+function drawPaths( ctx ) {
+    for ( var i = 0; i < nbrCars; i++ ) {
+        drawPath( ctx, paths[ i ] );
     }
 }
 
 window.onload = function() {
     createVars();
-    var c = document.getElementById("canvas");
-    var ctx = c.getContext("2d");
-    for (var i = 0; i < nbrCars; i++) {
-        createPath(i);
+    var c = document.getElementById( "canvas" );
+    var ctx = c.getContext( "2d" );
+    for ( var i = 0; i < nbrCars; i++ ) {
+        createPath( i );
     }
-    drawPaths(ctx);
+    drawPaths( ctx );
 };
 
 var move_cars = function() {
-    var c = document.getElementById("canvas");
-    var ctx = c.getContext("2d");
-    ctx.clearRect(0, 0, c.width, c.height);
-    drawPaths(ctx);
+    var c = document.getElementById( "canvas" );
+    var ctx = c.getContext( "2d" );
+    ctx.clearRect( 0, 0, c.width, c.height );
+    drawPaths( ctx );
     var msg = "";
-    for (var i = 0; i < nbrCars; i++) {
-        var s = Math.floor(Math.random() * 6) + 1;
-        move_car(ctx, i, s);
-        msg += "car " + (i + 1) + " [" + position[i].lap + "]\n";
+    for ( var i = 0; i < nbrCars; i++ ) {
+        moveCar( ctx, i );
+        msg += "car " + ( i + 1 ) + " [" + cars[ i ].lap + "]\n";
     }
-    setRaceInfo(msg);
+    setRaceInfo( msg );
 };
 
-var move_car = function(ctx, p, s) {
-    if (!s) s = 1;
-    var step = steps[position[p].segment];
-    var nbrSegments = steps.length;
-    position[p].t += step * s;
-    if (position[p].t > 1) {
-        position[p].segment++;
-        if (position[p].segment === nbrSegments) {
-            position[p].segment = 0;
-            position[p].lap++;
-        }
-        position[p].t -= 1.0;
-        // position[p].t = 0;
-    }
+var drawCarAt = function( ctx, car, x, y ) {
     ctx.beginPath();
-    ctx.strokeStyle = position[p].color;
-    var result = getPositionInPath(paths[p], position[p].segment, position[p].t);
-    ctx.moveTo(result.x, result.y);
-    ctx.arc(result.x, result.y, 3, 0, 2*Math.PI);
+    ctx.strokeStyle = car.color;
+    ctx.moveTo( x, y );
+    ctx.arc( x, y, 3, 0, 2 * Math.PI );
     ctx.stroke();
 };
 
-setInterval(move_cars, 200);
+var moveCar = function( ctx, carIdx ) {
+    var speed = Math.floor( Math.random() * cars[ carIdx ].spec.speed ) + 1;
+    var step = steps[ cars[ carIdx ].segment ];
+    var nbrSegments = steps.length;
+    cars[ carIdx ].t += step * speed;
+    if ( cars[ carIdx ].t > 1 ) {
+        cars[ carIdx ].segment++;
+        if ( cars[ carIdx ].segment === nbrSegments ) {
+            cars[ carIdx ].segment = 0;
+            cars[ carIdx ].lap++;
+        }
+        cars[ carIdx ].t -= 1.0;
 
+        // Cars[carIdx].t = 0;
+    }
+    var pos = getPositionInPath( paths[ carIdx ], cars[ carIdx ].segment, cars[ carIdx ].t );
+    drawCarAt( ctx, cars[ carIdx ], pos.x, pos.y );
+};
 
+setInterval( move_cars, 200 );
--- canvas/math.js
+++ canvas/math.js
@@ -1,8 +1,21 @@
-Point = function(x, y) {
-    if (!x) x = 0;
-    if (!y) y = 0;
-    return {x: x, y: y};
-};
+function Point( x, y ) {
+    if ( !x ) x = 0;
+    if ( !y ) y = 0;
+    return { x: x, y: y };
+}
+
+function drawBezier( ctx, bezier, points, resolution ) {
+    if ( !resolution ) resolution = 0.01;
+    var next;
+    var args = points.slice();
+    args.unshift( 0 );
+    for ( var x = 0.0; x <= 1.0; x += resolution ) {
+        args[ 0 ] = x;
+        next = bezier.apply( null, args );
+        ctx.lineTo( next.x, next.y );
+    }
+
+}
 
 /**
  * Linear Bezier curves.
@@ -13,27 +26,23 @@ Point = function(x, y) {
  * @param  {Point} c2 second control Point
  * @return {Point} result Point in the space
  */
-function getLineBezier(t, c1, c2) {
-    var B1 = function(t) {
-        return (1 - t);
+function getLineBezier( t, c1, c2 ) {
+    var B1 = function( t ) {
+        return ( 1 - t );
     };
 
-    var B2 = function(t) {
+    var B2 = function( t ) {
         return t;
     };
 
     var pos = new Point();
-    pos.x = c1.x * B1(t) + c2.x * B2(t);
-    pos.y = c1.y * B1(t) + c2.y * B2(t);
+    pos.x = c1.x * B1( t ) + c2.x * B2( t );
+    pos.y = c1.y * B1( t ) + c2.y * B2( t );
     return pos;
 }
 
-function drawLineBezier(ctx, c1, c2, resolution) {
-    if (!resolution) resolution = 0.01;
-    for ( var x = 0.0; x <= 1.0; x += resolution ) {
-        var next = getLineBezier(x, c1, c2);
-        ctx.lineTo(next.x, next.y);
-    }
+function drawLineBezier( ctx, c1, c2, resolution ) {
+    return drawBezier( ctx, getLineBezier, [ c1, c2 ], resolution );
 }
 
 /**
@@ -46,34 +55,30 @@ function drawLineBezier(ctx, c1, c2, resolution) {
  * @param  {Point} c3 third control Point
  * @return {Point} result Point in the space
  */
-function getCuadraticBezier(t, c1, c2, c3) {
-    var B1 = function(t) {
-        var result = (1-t)*(1-t);
+function getCuadraticBezier( t, c1, c2, c3 ) {
+    var B1 = function( t ) {
+        var result = ( 1 - t ) * ( 1 - t );
         return result;
     };
 
-    var B2 = function(t) {
-        var result = 2*t*(1-t);
+    var B2 = function( t ) {
+        var result = 2 * t * ( 1 - t );
         return result;
     };
 
-    var B3 = function(t) {
-        var result = t*t;
+    var B3 = function( t ) {
+        var result = t * t;
         return result;
     };
 
     var pos = new Point();
-    pos.x = c1.x * B1(t) + c2.x * B2(t) + c3.x * B3(t);
-    pos.y = c1.y * B1(t) + c2.y * B2(t) + c3.y * B3(t);
+    pos.x = c1.x * B1( t ) + c2.x * B2( t ) + c3.x * B3( t );
+    pos.y = c1.y * B1( t ) + c2.y * B2( t ) + c3.y * B3( t );
     return pos;
 }
 
-function drawCuadraticBezier(ctx, c1, c2, c3, resolution) {
-    if (!resolution) resolution = 0.01;
-    for ( var x = 0.0; x <= 1.0; x += resolution ) {
-        var next = getCuadraticBezier(x, c1, c2, c3);
-        ctx.lineTo(next.x, next.y);
-    }
+function drawCuadraticBezier( ctx, c1, c2, c3, resolution ) {
+    return drawBezier( ctx, getCuadraticBezier, [ c1, c2, c3 ], resolution );
 }
 
 /**
@@ -87,68 +92,29 @@ function drawCuadraticBezier(ctx, c1, c2, c3, resolution) {
  * @param  {Point} c4 fourth control Point
  * @return {Point} result Point in the space
  */
-function getCubicBezier(t, c1, c2, c3, c4) {
-    var B1 = function(t) {
-        return (1-t)*(1-t)*(1-t);
+function getCubicBezier( t, c1, c2, c3, c4 ) {
+    var B1 = function( t ) {
+        return ( 1 - t ) * ( 1 - t ) * ( 1 - t );
     };
 
-    var B2 = function(t) {
-        return 3*t*(1-t)*(1-t);
+    var B2 = function( t ) {
+        return 3 * t * ( 1 - t ) * ( 1 - t );
     };
 
-    var B3 = function(t) {
-        return 3*t*t*(1-t);
+    var B3 = function( t ) {
+        return 3 * t * t * ( 1 - t );
     };
 
-    var B4 = function(t) {
-        return t*t*t;
+    var B4 = function( t ) {
+        return t * t * t;
     };
 
     var pos = new Point();
-    pos.x = c1.x * B1(t) + c2.x * B2(t) + c3.x * B3(t) + c4.x * B4(t);
-    pos.y = c1.y * B1(t) + c2.y * B2(t) + c3.y * B3(t) + c4.y * B4(t);
+    pos.x = c1.x * B1( t ) + c2.x * B2( t ) + c3.x * B3( t ) + c4.x * B4( t );
+    pos.y = c1.y * B1( t ) + c2.y * B2( t ) + c3.y * B3( t ) + c4.y * B4( t );
     return pos;
 }
 
-function drawCubicBezier(ctx, c1, c2, c3, c4, resolution) {
-    if (!resolution) resolution = 0.01;
-    for ( var x = 0.0; x <= 1.0; x += resolution ) {
-        var next = getCubicBezier(x, c1, c2, c3, c4);
-        ctx.lineTo(next.x, next.y);
-    }
+function drawCubicBezier( ctx, c1, c2, c3, c4, resolution ) {
+    return drawBezier( ctx, getCubicBezier, [ c1, c2, c3, c4 ], resolution );
 }
-
-// window.onload = function() {
-//     var origin, next, x;
-//     var C1 = Point(50, 100);
-//     var C2 = Point(-50, 200);
-//     var C3 = Point(150, 400);
-//     var C4 = Point(500, 300);
-
-//     var c = document.getElementById("canvas");
-//     var ctx = c.getContext("2d");
-//     ctx.beginPath();
-//     ctx.moveTo(0, 0);
-//     drawLineBezier(ctx, Point(0, 0), Point(500, 0));
-//     // for ( x = 0.01; x <= 1; x += 0.01 ) {
-//     //     next = getLineBezier(x, Point(0, 0), Point(500, 0));
-//     //     ctx.lineTo(next.x, next.y);
-//     // }
-
-//     origin = getCuadraticBezier(0, C1, C2, C3);
-//     ctx.moveTo(origin.x, origin.y);
-//     drawCuadraticBezier(ctx, C1, C2, C3);
-//     // for ( x = 0.01; x <= 1; x += 0.01 ) {
-//     //     next = getCuadraticBezier(x, C1, C2, C3);
-//     //     ctx.lineTo(next.x, next.y);
-//     // }
-
-//     origin = getCubicBezier(0, C1, C2, C3, C4);
-//     ctx.moveTo(origin.x, origin.y);
-//     drawCubicBezier(ctx, C1, C2, C3, C4);
-//     // for ( x = 0.01; x <= 1; x += 0.01 ) {
-//     //     next = getCubicBezier(x, C1, C2, C3, C4);
-//     //     ctx.lineTo(next.x, next.y);
-//     // }
-//     ctx.stroke();
-// };
